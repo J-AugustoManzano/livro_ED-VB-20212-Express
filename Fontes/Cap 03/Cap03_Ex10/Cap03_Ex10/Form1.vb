a7b19b992fdd99ee6a38dd1d73036cf9dfa52ff8
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 23:01:21 -0300

    Add files via upload

---
+++ Fontes/Cap 03/Cap03_Ex10/Cap03_Ex10/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Mes As Integer
+        Mes = Val(TextBox1.Text)
+        Select Case Mes
+            Case 1
+                TextBox2.Text = "Janeiro"
+            Case 2
+                TextBox2.Text = "Fevereiro"
+            Case 3
+                TextBox2.Text = "Março"
+            Case 4
+                TextBox2.Text = "Abril"
+            Case 5
+                TextBox2.Text = "Maio"
+            Case 6
+                TextBox2.Text = "Junho"
+            Case 7
+                TextBox2.Text = "Julho"
+            Case 8
+                TextBox2.Text = "Agosto"
+            Case 9
+                TextBox2.Text = "Setembro"
+            Case 10
+                TextBox2.Text = "Outubro"
+            Case 11
+                TextBox2.Text = "Novembro"
+            Case 12
+                TextBox2.Text = "Dezembro"
+            Case Else
+                TextBox2.Text = "Mês Inválido"
+        End Select
+        TextBox1.Clear()
+        TextBox1.Focus()
+    End Sub
+End Class
---
+++ Fontes/Cap 03/Cap03_Ex11/Cap03_Ex11/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim TEXTO As String
+        Dim TITULO As String
+        Dim PEDIDO, NUMERO As Byte
+        Dim NUMERO_RANDOMICO As New Random()
+        TEXTO = "Entre um numero entre 1 e 5"
+        TITULO = "Tente sua Sorte"
+        NUMERO = NUMERO_RANDOMICO.Next(1, 6)
+        PEDIDO = Val(InputBox(TEXTO, TITULO, , 50, 70))
+        If (PEDIDO = 0) Then
+            MsgBox("Não houve entrada!", 48)
+        Else
+            If (PEDIDO = NUMERO) Then
+                MsgBox("Acertou - valor sorteado: " & Str(NUMERO), 64)
+            Else
+                MsgBox("Errou - valor sorteado: " & Str(NUMERO), 16)
+            End If
+        End If
+    End Sub
+End Class
