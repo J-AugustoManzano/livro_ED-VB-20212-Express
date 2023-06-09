b7106f7a4765429b932a4e31ded80647980fc4f4
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 22:53:31 -0300

    Add files via upload

---
+++ Fontes/Cap 03/Cap03_Ex04/Cap03_Ex04/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim NUMERO As Single
+        NUMERO = TextBox1.Text
+        If (NUMERO >= 20) And (NUMERO <= 90) Then
+            MsgBox("O valor está entre 20 e 90")
+        Else
+            MsgBox("O valor não está entre 20 e 90")
+        End If
+        TextBox1.Clear()
+        TextBox1.Focus()
+    End Sub
+End Class
