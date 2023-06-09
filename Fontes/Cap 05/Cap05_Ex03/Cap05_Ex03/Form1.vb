64720e6b56dfa7c919d8ffa9676908e8c373e73a
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 23:10:03 -0300

    Add files via upload

---
+++ Fontes/Cap 05/Cap05_Ex03/Cap05_Ex03/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim N As Long
+        N = Val(TextBox1.Text)
+        MsgBox("Fatorial = " & Str(Fatorial(N)))
+        TextBox1.Clear()
+        TextBox1.Focus()
+    End Sub
+
+    Function Fatorial(ByVal X As Long) As Long
+        Dim I, F As Long
+        F = 1
+        For I = 1 To X
+            F = F * I
+        Next
+        Fatorial = F
+    End Function
+
+End Class
