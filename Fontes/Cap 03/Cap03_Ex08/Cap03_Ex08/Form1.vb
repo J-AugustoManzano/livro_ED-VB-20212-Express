1f6bedfac914f57ebe14e2282ed8d9dcee26bab7
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 23:00:07 -0300

    Add files via upload

---
+++ Fontes/Cap 03/Cap03_Ex08/Cap03_Ex08/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim A, B, C As Single
+        A = Val(TextBox1.Text)
+        B = Val(TextBox2.Text)
+        C = Val(TextBox3.Text)
+        If (A < B + C) And (B < A + C) And (C < A + B) Then
+            If (A = B) And (B = C) Then
+                MsgBox("Triângulo Equilátero")
+            Else
+                If (A = B) Or (A = C) Or (C = B) Then
+                    MsgBox("Triângulo Isósceles")
+                Else
+                    MsgBox("Triângulo Escaleno")
+                End If
+            End If
+        Else
+            MsgBox("Não é triângulo")
+        End If
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox1.Focus()
+    End Sub
+End Class
---
+++ Fontes/Cap 03/Cap03_Ex09/Cap03_Ex09/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim R, A, B As Single
+        A = Val(TextBox1.Text)
+        B = Val(TextBox2.Text)
+        If (RadioButton1.Checked) Then
+            R = A + B
+        End If
+        If (RadioButton2.Checked) Then
+            R = A - B
+        End If
+        If (RadioButton3.Checked) Then
+            R = A * B
+        End If
+        If (RadioButton4.Checked) Then
+            If (B = 0) Then
+                MsgBox("ERRO – Divisor igual a zero!")
+            Else
+                R = A / B
+            End If
+        End If
+        TextBox3.Text = R
+        TextBox1.Focus()
+    End Sub
+End Class
