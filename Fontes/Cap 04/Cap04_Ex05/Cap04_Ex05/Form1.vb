9878e7a5e7cd65e929940e965dfc346372096a22
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 23:08:04 -0300

    Add files via upload

---
+++ Fontes/Cap 04/Cap04_Ex05/Cap04_Ex05/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FAT = 1, N, I As Long
+        N = Val(TextBox1.Text)
+        I = 1
+        Do
+            FAT = FAT * I
+            If (I >= N) Then
+                Exit Do
+            End If
+            I = I + 1
+        Loop
+        MsgBox("Fatorial = " & Str(FAT), 64, "Resultado da Fatorial")
+        TextBox1.Clear()
+        TextBox1.Focus()
+    End Sub
+End Class
---
+++ Fontes/Cap 04/Cap04_Ex06/Cap04_Ex06/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FAT = 1, N, I As Long
+        N = Val(TextBox1.Text)
+        For I = 1 To N Step 1
+            FAT = FAT * I
+        Next I
+        MsgBox("Fatorial = " & Str(FAT), 64, "Resultado da Fatorial")
+        TextBox1.Clear()
+        TextBox1.Focus()
+    End Sub
+End Class
