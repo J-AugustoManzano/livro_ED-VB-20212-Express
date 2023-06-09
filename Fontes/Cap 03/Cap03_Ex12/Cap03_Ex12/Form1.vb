84dd3b785a0f366f6b38525d60aff88cdd2b3557
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 23:02:24 -0300

    Add files via upload

---
+++ Fontes/Cap 03/Cap03_Ex12/Cap03_Ex12/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim MSG, RESP As String
+        MSG = "Entre algo e acione um botão."
+        RESP = InputBox(MSG)
+        If (Len(RESP) = 0) Then
+            MsgBox("Botão 'Cancelar' acionado.")
+        Else
+            MsgBox("Botão 'OK' acionado.")
+        End If
+    End Sub
+End Class
---
+++ Fontes/Cap 03/Cap03_Ex13/Cap03_Ex13/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim MSG As String
+        Dim RESP As Integer
+        MSG = "Selecione um botão"
+        RESP = MsgBox(MSG, vbOKCancel)
+        If (RESP = vbOK) Then
+            MsgBox("Acionou o botão 'OK'.")
+        End If
+        If (RESP = vbCancel) Then
+            MsgBox("Acionou o botão 'Cancelar'.")
+        End If
+    End Sub
+End Class
